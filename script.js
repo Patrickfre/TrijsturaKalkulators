c88dce23c1e2d0624ba9b2dbe02daab5b3ad825f
Patrickfre <97429051+Patrickfre@users.noreply.github.com>
2022-01-24 20:35:13 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,74 @@
+function nolasa() {
+    const malas = document.getElementsByClassName("mala");
+    const m1 = parseFloat(mala1.value);
+    const m2 = parseFloat(mala2.value);
+    const m3 = parseFloat(mala3.value);
+    if (m1 > 0 && m2 > 0 && m3 > 0) {
+        console.log({ m1, m2, m3 });
+        return { m1, m2, m3 };
+    } else {
+        console.log(false);
+        return false;
+    }
+}
+function perimetrs(m1, m2, m3) {
+    const m = nolasa();
+    m1 = m.m1; m2 = m.m2; m3 = m.m3;
+    console.log(m1, m2, m3);
+    const p = m1 + m2 + m3;
+    console.log(p);
+    return p;
+}
+function laukums(m1, m2, m3) {
+    const m = nolasa();
+    m1 = m.m1; m2 = m.m2; m3 = m.m3;
+    const pusper = perimetrs(m1, m2, m3) / 2;
+    let laukums = Math.sqrt(pusper * (pusper - m1) * (pusper - m2) * (pusper - m3));
+    console.log(laukums);
+    return laukums;
+}
+function irTrijsturis(m1, m2, m3) {
+    if (m1 < m2 + m3 && m2 < m1 + m3 && m3 < m1 + m2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+function rezultats() {
+    const m = nolasa();
+    m1 = m.m1; m2 = m.m2; m3 = m.m3;
+    console.log(m1, m2, m3);
+    if (!nolasa()) {
+        t = "Nepareizi ievaddati, trijstūra malu vērtībām jābūt lielākām par 0";
+    } else {
+        if (irTrijsturis(m1, m2, m3) == false) {
+            t = "Trijstūris neeksistē"
+        } else {
+            t = "Trijstūris ar malu garu garumiem " + m1 + ", " + m2 + ", " + m3 + " ir reāls.";
+            if (m1 == m2 && m2 == m3) {
+                const p = perimetrs(m1, m2, m3);
+                const s = Math.round(laukums(m1, m2, m3) * 100) / 100;
+                t += "Tas ir vienādmalu trijstūris.";
+            } else {
+                if(m1 == m2 || m2 == m3 || m3==m2){
+                    const p = perimetrs(m1, m2, m3);
+                    const s = Math.round(laukums(m1, m2, m3) * 100) / 100;
+                    t += "Tas ir vienādsānu trijstūris.";
+                }
+            }
+            const p = perimetrs(m1, m2, m3);
+            const s = Math.round(laukums(m1, m2, m3) * 100) / 100;
+            t += " Perimetrs ir " + p + " un laukums ir " + s + "."
+        }
+    }
+    console.log(t);
+    return t;
+}
+function izvadaTekstu() {
+    const teksts = rezultats();
+    console.log(teksts);
+    const sakne = document.getElementById("izvade");
+    const raksti = document.createElement("p");
+    sakne.innerHTML = teksts;
+    sakne.appendChild(raksti);
+}
